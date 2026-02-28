f8267d02836db0f46b709c6389f421492717274f
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-02-28 13:07:47 -0500

    add gif

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const img = document.querySelector('img');
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=rNi0yvET0y0zGm4PYKzRKcUWbL5V4Fw5&s=cats')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      img.src = response.data.images.original.url;
+    });
+ 
